639e250c71c50aaf6d47ced4a97a0634c0a681d0
deathofnine <96811559+deathofnine@users.noreply.github.com>
2026-05-19 23:34:11 +0800

    feat：新增互斥锁，防止脏数据

--- rpc_server_project/rpc_server/dht11.c
+++ rpc_server_project/rpc_server/dht11.c
@@ -8,7 +8,10 @@
 #include <pthread.h>
 static int fd;
 static char g_humi,g_temp;
-
+pthread_mutex_t data_mutex;  
+//------------------------
+//DHT11采集线程
+//-----------------------
 void *dht11_thread(void *arg)
 {
     char buf[2];
@@ -16,30 +19,52 @@ void *dht11_thread(void *arg)
     {
         if (2 == read(fd, buf, 2))
         {
+            pthread_mutex_lock(&data_mutex);
             g_humi = buf[0];
             g_temp = buf[1];
+            pthread_mutex_unlock(&data_mutex);
             sleep(1);
         }
     }
 
 }
-
+}
+//-------------------------
+//初始化
+//-----------------------
 void mydht11_init(void)
 {
     pthread_t tid1;
+    pthread_mutex_init(&data_mutex, NULL);
     fd = open("/dev/mydht11", O_RDWR | O_NONBLOCK);
     if (fd >= 0)
     {
         int ret = pthread_create(&tid1,NULL,dht11_thread,NULL);//创建线程
     }
 }
-
+//-------------------------
+//对外读取接口
+//-----------------------
 int mydht11_read(char *humi, char *temp)
 {
-
+        pthread_mutex_lock(&data_mutex);
         *humi = g_humi;
         *temp = g_temp;
+        pthread_mutex_unlock(&data_mutex);
         return 0;
 }
 
 
+// -----------------------
+//统一销毁释放
+//-------------------------
+void mydht11_deinit(void)
+{
+
+    if(fd >= 0)
+    {
+        close(fd);
+        fd = -1;
+    }
+    pthread_mutex_destroy(&data_mutex);
+}
